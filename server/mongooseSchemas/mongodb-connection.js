bd80cd868bc3e22dc23576064f7f059a59540c45
Bryan Muller <starwarsgeek0975@gmail.com>
2017-12-26 21:36:31 -0500

    update Mongo credentials for more secure remote connection.

--- server/mongooseSchemas/mongodb-connection.js
+++ server/mongooseSchemas/mongodb-connection.js
@@ -1,7 +1,14 @@
 import Mongoose from 'mongoose'
 Mongoose.Promise = global.Promise;
-Mongoose.connect('mongodb://localhost/econ-sim', {useMongoClient: true});
-//Mongoose.connect('mongodb://dev:colinisdecent@ds229435.mlab.com:29435/econsimdev', {useMongoClient: true});
+const MongoURL = process.env.MongoConnectionURL;
+const MongoUser = process.env.MongoConnectionUser;
+const MongoPwd = process.env.MongoConnectionPwd;
+
+// Mongoose.connect('mongodb://localhost/econ-sim', {useMongoClient: true});
+// Mongoose.connect('mongodb://dev:colinisdecent@ds229435.mlab.com:29435/econsimdev', {useMongoClient: true});
+
+Mongoose.connect(`mongodb://${MongoUser}:${MongoPwd}@${MongoURL}`, {useMongoClient: true});
+
 export const mongo = Mongoose.connection;
 mongo.on('error', console.error.bind(console, "connection err:"));
 mongo.once('open' ,() =>{
